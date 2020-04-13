7f7a0d4b0c8138ac361b85c1b0b872da10311c58
AntoineL <antoine.laroche@akinox.com>
2020-04-13 16:09:53 -0400

    fix and include just image file

--- FileRenameBasedOnDirectory.ps1
+++ FileRenameBasedOnDirectory.ps1
@@ -1,11 +1,10 @@
 $location = Get-Location
 $folderName = $location | Select-Object | ForEach-Object { $_.ProviderPath.Split("\")[-1] }
-$listOfFilesToRename = Get-ChildItem -Path $location -Exclude *.ps1
+$listOfFilesToRename = Get-ChildItem -Path $location -Include ('*.bmp', '*.jpg', '*.png') -Recurse
 $i = 1
 $listOfFilesToRename | ForEach-Object {
     $index = '{0:d4}' -f $i
-    $extension = $_.split(".")[1]
-    [string]$newName = $index + "_" + $folderName + "." + $extension
+    [string]$newName = $index + "_" + $folderName + $_.extension
     Rename-Item -Path $_ -NewName $newName -Verbose
     $i++
 }
